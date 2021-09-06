e266dfedc47aa2a54b29136143fee636b6caf0fc
Dahlton <dahlton@linkd.dev>
2021-09-06 12:06:12 -0400

    set up onscroll

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,52 +1,79 @@
-import React, { Fragment, useRef } from 'react'
+import React, { Fragment, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom'
 export const Navbar = () => {
-    const navBar = useRef(null);
-    const onClick = () => {
-        const navbar = navBar.current
-        !navbar.classList.contains('bg-main') ? navbar.classList.add('bg-main') : navbar.classList.remove('bg-main')
-    }
-    return (
-        <Fragment>
-            <nav id="navbar" ref={navBar} className="navbar sticky-top navbar-expand-lg navbar-dark p-0">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href=" ">
-                    <img src="/logo-larger-bg.png" alt="" width="30" height="36" className="d-inline-block align-text-center"/>
-                    Built With Brains
-                    </a>
-                    <button className="navbar-toggler" id="navBtn" onClick={onClick} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item ms-5">
-                                <a className="nav-link" href="/">Home</a>
-                            </li>
-                            <li className="nav-item ms-5">
-                                <a className="nav-link" href="/coaches">Coaches</a>
-                            </li>
-                            <li className="nav-item ms-5">
-                                <a className="nav-link" href="/training">Training</a>
-                            </li>
-                            <li className="nav-item ms-5">
-                                <a className="nav-link" href="/shop">Shop apparel</a>
-                            </li>
-                            <li className="nav-item ms-5">
-                                <a className="nav-link" href="/contact">Contact Us</a>
-                            </li>
-                        </ul>
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item ms-5">
-                                <a className="btn nav-btn" id="log-in-btn" href=" ">Login</a>
-                            </li>
-                            <li className="nav-item ms-5">
-                                <a className="btn nav-btn" id="sign-up-btn" href=" ">Sign-Up</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </Fragment>
-    )
+	const navBar = useRef(null);
 
-    
-}
+	const onClick = () => {
+		const navbar = navBar.current;
+		!navbar.classList.contains('bg-main') ? navbar.classList.add('bg-main') : navbar.classList.remove('bg-main');
+	};
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const navbar = navBar.current;
+			if (window.scrollY >= 600) {
+				if (!navbar.classList.contains('bg-main')) navbar.classList.add('bg-main');
+			} else {
+				if (navbar.classList.contains('bg-main')) navbar.classList.remove('bg-main');
+			}
+		};
+		window.addEventListener('scroll', handleScroll, { passive: true });
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+	return (
+		<Fragment>
+			<nav id='navbar' ref={navBar} className='navbar fixed-top navbar-expand-lg navbar-dark p-0'>
+				<div className='container-fluid mb-1'>
+					<a className='navbar-brand' href=' '>
+						<img src='/logo-larger-bg.png' alt='' width='30' height='36' className='d-inline-block me-2' />
+						Built With Brains
+					</a>
+					<button className='navbar-toggler' id='navBtn' onClick={onClick} type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+						<span className='navbar-toggler-icon'></span>
+					</button>
+					<div className='collapse navbar-collapse' id='navbarNav'>
+						<ul className='navbar-nav'>
+							<li className='nav-item ms-5'>
+								<Link className='nav-link' to='/'>
+									Home
+								</Link>
+							</li>
+							<li className='nav-item ms-5'>
+								<Link className='nav-link' to='/coaches'>
+									Coaches
+								</Link>
+							</li>
+							<li className='nav-item ms-5'>
+								<Link className='nav-link' to='/training'>
+									Training
+								</Link>
+							</li>
+							<li className='nav-item ms-5'>
+								<Link className='nav-link' to='/shop'>
+									Shop apparel
+								</Link>
+							</li>
+							<li className='nav-item ms-5'>
+								<Link className='nav-link' to='/contact'>
+									Contact Us
+								</Link>
+							</li>
+						</ul>
+						<ul className='navbar-nav ms-auto'>
+							<li className='nav-item ms-5'>
+								<Link className='btn nav-btn' id='log-in-btn' to=' '>
+									Login
+								</Link>
+							</li>
+							<li className='nav-item ms-5'>
+								<Link className='btn nav-btn' id='sign-up-btn' to=' '>
+									Sign-Up
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</Fragment>
+	);
+};
