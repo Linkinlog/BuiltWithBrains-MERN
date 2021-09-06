9cafc028e199ba9c911993772ed245c535524b9f
Dahlton <dahlton@linkd.dev>
2021-09-05 20:59:51 -0400

    set up app with main and coaches routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Landing } from './components/Landing';
+import { Navbar } from './components/layout/Navbar';
+import { CoachesMain } from './components/Coaches/CoachesMain';
+import { MainContextProvider } from './Context/MainContext';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<MainContextProvider>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path='/' component={Landing} />
+					<Route exact path='/coaches' component={CoachesMain} />
+				</Switch>
+			</Router>
+		</MainContextProvider>
+	);
 }
 
 export default App;
