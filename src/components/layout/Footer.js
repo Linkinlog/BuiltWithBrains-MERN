a96a4597b8aefe98c0e9df5dee3cbdb86c36ef09
Dahlton <dahlton@linkd.dev>
2021-09-06 12:06:43 -0400

    Made footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+export const Footer = () => (
+	<Fragment>
+		<div className="gap"></div>
+		<footer className='footer'>
+			<p>Built With Brains</p>
+		</footer>
+	</Fragment>
+);
