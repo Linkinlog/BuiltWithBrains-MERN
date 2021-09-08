337e293ddb1dede6a2e97a967156da49cecce4e2
Dahlton <dahlton@linkd.dev>
2021-09-08 15:09:17 -0400

    border and grammar

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -10,8 +10,8 @@ export const NotFound = () => {
 						<img src='https://images.unsplash.com/photo-1578328819058-b69f3a3b0f6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80' className='card-img-top' alt='...' />
 						<div className='card-body'>
 							<h5 className='card-title'>Error 404!</h5>
-							<p className='card-text'>Sorry about that! Looks like we couldn't find that component, please click here to go home</p>
-							<Link to='/' className="btn btn-info">Home</Link>
+							<p className='card-text'>Sorry about that! Looks like we couldn't find that component, please click below to go home</p>
+							<Link to='/' className="btn btn-info hotBorder">Home</Link>
 						</div>
 					</div>
 				</div>
