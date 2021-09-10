31725514edb263e028ba1dc0b59f12c1fff9b6c9
Dahlton <dahlton@linkd.dev>
2021-09-10 14:00:00 -0400

    added proper auth and get route

--- routes/coaches.js
+++ routes/coaches.js
@@ -1,20 +1,27 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 const Coach = require('../models/Coach');
-
+const config = require('config')
 /* GET coaches listing. */
-router.get('/', function (req, res, next) {
-  try {
-    
-  } catch (error) {
-    
-  }
-});
+router.get('/', async (req, res, next) => {
+		try {
+			const coaches = await Coach.find({});
+			res.send(coaches);
+		} catch (error) {
+			console.error(error);
+			res.json({ msg: 'Server error' });
+		}
+	});
 
 // @POST /
 // Make new coach
 // Private
-router.post('/', async (req, res, next) => {
+router.post('/',auth, async (req, res, next) => {
+	for(let admin of config.get('admins')){
+		if(req.user.id !== admin)
+		return res.json({msg: 'Admins only.'})
+	}
 	try {
 		const coach = new Coach(req.body.coach);
 		await coach.save();
