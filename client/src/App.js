afb2dd9b0caeb49aa11c36cb9e30f435542b8003
Dahlton <dahlton@linkd.dev>
2021-09-11 00:56:21 -0400

    added coach context to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,28 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Landing } from './components/Landing';
 import { Navbar } from './components/layout/Navbar';
 import { CoachesMain } from './components/Coaches/CoachesMain';
-import { MainContextProvider } from './Context/MainContext';
+import CoachState from './Context/coaches/CoachState';
 import { NotFound } from './components/layout/NotFound';
-
+import { Overview } from './components/Overview';
+import { About } from './components/About';
+import { Footer } from './components/layout/Footer';
 const App = () => {
 	return (
-		<MainContextProvider>
+		<CoachState>
 			<Router>
 				<Navbar />
-				<Switch>
-					<Route exact path='/' component={Landing} />
-					<Route exact path='/coaches' component={CoachesMain} />
-					<Route component={NotFound} />
-				</Switch>
+				<div className='container-fluid'>
+					<Switch>
+						<Route exact path='/' component={Landing} />
+						<Route exact path='/coaches' component={CoachesMain} />
+						<Route exact path='/overview' component={Overview} />
+						<Route exact path='/about' component={About} />
+						<Route component={NotFound} />
+					</Switch>
+				</div>
+				<Footer />
 			</Router>
-		</MainContextProvider>
+		</CoachState>
 	);
 };
 
