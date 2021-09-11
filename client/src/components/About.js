fbbad6f60f02970b2458e29cf986a7336ec70595
Dahlton <dahlton@linkd.dev>
2021-09-11 00:55:04 -0400

    Set up about page

---
+++ client/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const About = () => {
+	return (
+		<div className='container'>
+			<div className='row about hotBorder'>
+				<div className='col-md-10 offset-md-1'>
+					<div className=' text-center lh-lg p-5'>
+						<h1 className='mb-5'>
+							<strong>About Built with Brains</strong>
+						</h1>
+						Fitness isn’t about the “20-minute Abs” or the “5 must-do exercises to grow your legs”. It is about making sustainable lifestyle changes that can be maintained over a long period of time. Changes that fit into an individual’s specific lifestyle. There is no cookie cutter plan for fitness. Not everyone can eat 6 meals a day or heat up meals at work. Not everyone has the time to lift 5 days a week or spend hours doing cardio. The truth is that no one should be doing the exact same thing as another person when it comes to nutrition and fitness because our bodies are all different and will respond differently to different things. The goal of Built with Brains is to develop plans that fit your life and your needs so that you are set up for success in reaching the goals that have been set. One of our motto’s here at BWB is that the best personal trainer is the one you don’t have for the rest of your life. Our goal isn’t for you to rely on us forever but rather to educate you on how to make healthier lifestyle changes to be a better you. There is a significant lack of education on nutrition and fitness in this industry and if we can help just one person lift smarter and eat better then we have succeeded. What’s stopping you from reaching our goals? Fill out our free consultation form today to see if BWB is a good fit for you!
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
