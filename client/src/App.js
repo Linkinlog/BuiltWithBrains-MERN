e5714b6cac8cc0db96f280225cff5aa53b2594aa
Dahlton <dahlton@linkd.dev>
2021-09-17 07:59:11 -0400

    set up auth and alert more for main app

--- client/src/App.js
+++ client/src/App.js
@@ -10,25 +10,37 @@ import { CoachesMain } from './components/Coaches/CoachesMain';
 import { Overview } from './components/Overview';
 import { About } from './components/About';
 import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
+import { Logout } from './components/auth/Logout';
 // Contexts
 import { AuthState } from './Context/auth/AuthState';
 import CoachState from './Context/coaches/CoachState';
+import AlertState from './Context/alert/AlertState';
+// Utils
+import { setAuthToken } from './scripts/utils/setAuthToken';
 
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
 const App = () => {
 	return (
 		<AuthState>
 			<CoachState>
-				<Router>
-					<Navbar />
-					<Switch>
-						<Route exact path='/' component={Landing} />
-						<Route exact path='/coaches' component={CoachesMain} />
-						<Route exact path='/overview' component={Overview} />
-						<Route exact path='/about' component={About} />
-						<Route exact path='/register' component={Register} />
-						<Route component={NotFound} />
-					</Switch>
-				</Router>
+				<AlertState>
+					<Router>
+						<Navbar />
+						<Switch>
+							<Route exact path='/' component={Landing} />
+							<Route exact path='/coaches' component={CoachesMain} />
+							<Route exact path='/overview' component={Overview} />
+							<Route exact path='/about' component={About} />
+							<Route exact path='/register' component={Register} />
+							<Route exact path='/login' component={Login} />
+							<Route exact path='/logout' component={Logout} />
+							<Route component={NotFound} />
+						</Switch>
+					</Router>
+				</AlertState>
 			</CoachState>
 		</AuthState>
 	);
