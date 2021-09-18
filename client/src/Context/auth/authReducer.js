6100073fe156da48e13c78dfa6850739fa42ac5f
Dahlton <dahlton@linkd.dev>
2021-09-18 07:48:14 -0400

    added login/loaded/logout/fails to reducer

--- client/src/Context/auth/authReducer.js
+++ client/src/Context/auth/authReducer.js
@@ -1,21 +1,42 @@
-import {REGISTER_FAIL, REGISTER_SUCCESS} from '../types'
+import { CLEAR_ERRORS, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT, REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED } from '../types';
 
 // eslint-disable-next-line
-export default ( state, action ) => {
-	switch(action.type){
-		case REGISTER_SUCCESS:
-			localStorage.setItem('token', action.payload.token)
+export default (state, action) => {
+	switch (action.type) {
+		case USER_LOADED:
 			return {
 				...state,
-				...action.payload
+				isAuthenticated: true,
+				loading: false,
+				user: action.payload,
+			};
+		case REGISTER_SUCCESS:
+		case LOGIN_SUCCESS:
+			localStorage.setItem('token', action.payload.token);
+			return {
+				...state,
+				...action.payload,
+				isAuthenticated: true,
+				loading: false,
+			};
+		case REGISTER_FAIL:
+		case LOGIN_FAIL:
+		case LOGOUT:
+			localStorage.removeItem('token');
+			return {
+				...state,
+				token: null,
+				isAuthenticated: false,
+				loading: false,
+				user: null,
+				error: action.payload,
+			};
+		case CLEAR_ERRORS:
+			return {
+				...state,
+				error: null
 			}
-			case REGISTER_FAIL:
-				localStorage.removeItem('token')
-				return {
-					...state,
-					token : null
-				}
 		default:
-			return state
+			return state;
 	}
-}
+};
