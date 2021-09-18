698c290786986cbd072d7ebbb5a547c412f7d1cc
Dahlton <dahlton@linkd.dev>
2021-09-18 07:45:54 -0400

    setup auth state

--- client/src/Context/auth/AuthState.js
+++ client/src/Context/auth/AuthState.js
@@ -2,43 +2,121 @@ import React, { useReducer } from 'react';
 import { AuthContext } from './authContext';
 import authReducer from './authReducer';
 import axios from 'axios';
-import { REGISTER_FAIL, REGISTER_SUCCESS } from '../types';
-
+import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, CLEAR_ERRORS, LOGOUT } from '../types';
+import { setAuthToken } from '../../scripts/utils/setAuthToken';
 export const AuthState = (props) => {
 	const initialState = {
-		token : localStorage.getItem('token'),
+		token: localStorage.getItem('token'),
+		isAuthenticated: null,
+		user: null,
+		loading: true,
+		error: null,
 	};
 
-	const register = async user => {
+	// Load user
+	const loadUser = async () => {
+		setAuthToken(localStorage.token);
+		try {
+			const res = await axios.get('/login');
+			dispatch({
+				type: USER_LOADED,
+				payload: res.data,
+			});
+		} catch (error) {
+			dispatch({ type: AUTH_ERROR });
+		}
+	};
+
+	// Register user
+	const register = async (user) => {
 		const config = {
 			headers: {
-				'Content-Type': 'application/json'
-			}
-		}
+				'Content-Type': 'application/json',
+			},
+		};
 		try {
-			const res = await axios.post( '/login/register' , user , config )
+			user.username = user.username.toLowerCase();
+			const res = await axios.post('/login/register', user, config);
 			dispatch({
 				type: REGISTER_SUCCESS,
-				payload: res.data
-			})
+				payload: res.data,
+			});
+			loadUser();
 		} catch (error) {
+			if (error.response.data.errors) {
+				for (let message of error.response.data.errors) {
+					dispatch({
+						type: REGISTER_FAIL,
+						payload: message.msg,
+					});
+				}
+			}
 			dispatch({
 				type: REGISTER_FAIL,
-				payload: error.response.data.msg
-			})
+				payload: error.response.data.msg,
+			});
 		}
-	}
+	};
+
+	// Log in user
+	const login = async (user) => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		try {
+			user.username = user.username.toLowerCase();
+			const res = await axios.post('/login', user, config);
+			dispatch({
+				type: LOGIN_SUCCESS,
+				payload: res.data,
+			});
+			loadUser();
+		} catch (error) {
+			if (error.response.data.errors) {
+				for (let message of error.response.data.errors) {
+					dispatch({
+						type: LOGIN_FAIL,
+						payload: message.msg,
+					});
+				}
+			}
+			dispatch({
+				type: LOGIN_FAIL,
+				payload: error.response.data.msg,
+			});
+		}
+	};
+
+	// Log out user
+	const logout = () => {
+		dispatch({ type: LOGOUT });
+	};
+
+	// Clear errors
+	const clearErrors = () => {
+		dispatch({ type: CLEAR_ERRORS });
+	};
 
 	const [state, dispatch] = useReducer(authReducer, initialState);
-	
+
 	return (
-		<AuthContext.Provider 
+		<AuthContext.Provider
 			value={{
-				token : state.token,
+				token: state.token,
+				isAuthenticated: state.isAuthenticated,
+				user: state.user,
+				loading: state.loading,
+				error: state.error,
 				register,
+				loadUser,
+				login,
+				logout,
+				clearErrors,
 			}}
 		>
 			{props.children}
 		</AuthContext.Provider>
-	)
-}
+	);
+};
