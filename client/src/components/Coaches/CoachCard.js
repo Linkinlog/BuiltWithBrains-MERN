2102e5010080f9df3bd5f5af7dcdf1343607b39f
Dahlton <dahlton@linkd.dev>
2021-09-20 07:17:16 -0400

    removed extra key, elongated cards, gitignore

--- client/src/components/Coaches/CoachCard.js
+++ client/src/components/Coaches/CoachCard.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { v4 as uuidv4 } from 'uuid'
 
 export const CoachCard = (coach) => {
 	const {URL, name, role, years} = coach
 	return (
-		<div key={uuidv4()} className='col pb-5'>
+		<div className='col pb-5'>
 		<div className='card coach-carder hotBorder text-center'>
-			<img src={URL} className='card-img-top rounded' height='300px' width='100px' alt={name} />
+			<img src={URL} className='card-img-top img-fluid rounded' style={{height:"35rem"}} width='100px' alt={name} />
 			<div className='card-body'>
 				<p className='card-text'>
 					{name} is our {role}, and they have been with us for over {years} years we look forward to having you meet us!
